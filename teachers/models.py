3008fb17c583d4e502f14a286ee7b79e8fca3e10
Bexruz Sayfiddinov <75200506+Winchestor2001@users.noreply.github.com>
2024-02-02 17:03:47 +0500

    Update models.py
    edit teacher_queue to IntegerField

--- teachers/models.py
+++ teachers/models.py
@@ -38,39 +38,7 @@ class Teacher(models.Model):
         ('Ichki o\'rindosh', 'Ichki o\'rindosh'),
         ('Tashqi o\'rindosh', 'Tashqi o\'rindosh'),
     )
-
-    CHOICES3=(
-        ('1','1'),
-        ('2','2'),
-        ('3','3'),
-        ('4','4'),
-        ('5','5'),
-        ('6','6'),
-        ('7','7'),
-        ('8','8'),
-        ('9','9'),
-        ('10','10'),
-        ('11','11'),
-        ('12','12'),
-        ('13','13'),
-        ('14','14'),
-        ('15','15'),
-        ('16','16'),
-        ('17','17'),
-        ('18','18'),
-        ('19','19'),
-        ('20','20'),
-        ('21','21'),
-        ('22','22'),
-        ('23','23'),
-        ('24','24'),
-        ('25','25'),
-        ('26','26'),
-        ('27','27'),
-        ('28','28'),
-        ('29','29'),
-        ('30','30'),
-        )
+    
     teacher_id = models.OneToOneField(User, on_delete=models.CASCADE)
     rank = models.CharField(max_length=255, blank=True, null=True, choices=CHOICES)
     type = models.CharField(max_length=255, blank=True, null=True, choices=CHOICES2)
@@ -81,7 +49,7 @@ class Teacher(models.Model):
     facebook = models.CharField(max_length=255, default='/')
     avatar = models.ImageField(upload_to='avatar/', blank=True, null=True)
     subjects = models.ManyToManyField(Subject)
-    teacher_queue=models.CharField(max_length=255, choices=CHOICES3,blank=True)
+    teacher_queue=models.IntegerField(default=0)
 
     def __str__(self):
         return '{} - {}'.format(self.rank, self.type)
